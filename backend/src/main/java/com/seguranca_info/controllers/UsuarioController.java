ca5e4ba560407d02254e6ce92a731df024f3ef99
Mateus de Sousa <mateus69107@gmail.com>
2024-09-08 19:25:08 -0300

    :fire: rm: removendo importações não utilizadas

--- backend/src/main/java/com/seguranca_info/controllers/UsuarioController.java
+++ backend/src/main/java/com/seguranca_info/controllers/UsuarioController.java
@@ -2,10 +2,8 @@ package com.seguranca_info.controllers;
 
 import java.util.List;
 
-import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
