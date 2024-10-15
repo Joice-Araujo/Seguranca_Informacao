734c1410a308516020531953afde197521bd8dd8
Mateus de Sousa <mateus69107@gmail.com>
2024-10-15 18:47:03 -0300

    :bugfix: corrigindo o componente de checkbox

--- frontend/src/Componentes/inputCheckbox.tsx
+++ frontend/src/Componentes/inputCheckbox.tsx
@@ -1,22 +1,31 @@
 interface InputProps {
     label: string;
     type: string;
-    value: string;
+    value: boolean;
     setValue: Function;
     inputClassName: string;
     labelClassName: string;
 }
 
 export function Check(props: InputProps) {
+
+    const handleCheckBox = () => {
+        props.value == true ? props.setValue(false) : props.setValue(true)
+    }
+
     return (
         <div className="flex flex-row space-x-2">
             <div>
-            <input type={props.type} onChange={e => props.setValue(e.target.value)}
-                value={props.value} className={props.inputClassName} />
+                <input type={props.type} onChange={() => {
+                    handleCheckBox()
+                }}
+                 className={props.inputClassName}
+                 checked={props.value}
+                 />
             </div>
 
             <div>
-            <label className={props.labelClassName}>{props.label}</label>
+                <label className={props.labelClassName}>{props.label}</label>
             </div>
         </div>
     );
